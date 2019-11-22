8f2cba4ec1553615aa92566da09abf8bbc33ca1d
Vinicius <viniciushenriquevaz@hotmail.com>
2019-11-22 12:45:59 -0300

    parcial

--- components/PlayListInfo/index.js
+++ components/PlayListInfo/index.js
@@ -2,22 +2,24 @@ import axios from 'axios'
 import { connect } from 'react-redux'
 import R from 'ramda'
 
-import { playListInfo } from '../../utils/routes'
+import { playListInfoSpotify, playListInfoDeezer } from '../../utils/routes'
 import { fetchData } from '../../ducks/requests'
 import MusicList from '../MusicList'
 
-const token = 'BQCC7PCp_9QgLnmwtX1qYLaU1pKJ4Xfb1P_7o0XndJaRfAc9PrHrswztu-TO-9OOUYxiDXJ62C46PdnjS3MSc7RD2rxb1jchFnECx0-RLA3N6kaR3-tpvl6Wppr8Df1r1JeCzEm3celxhB57kqqT2hM9'
-const spotifyPlaylist = '7mqbR7yHdisOwfKsaJtdLi'
+const token = 'BQDg1CLpKWUHdCSCd6ACgCBMxjaTw3RtKgEkgm8V_NkYHrTWzl-mNd-rM7JX3YnI5Thdi_w4Z5d5OBYOmf2byEXaY6Gou5rWYGUtvqImrF0vAJOXkjX-nfypaDYDWDnEgmgUyeKOf9LW61oIUzjc_Rp3'
 
 class PlayListInfo extends React.Component {
   render() {
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    const name = R.pathOr('', ['playList', 'data', 'name'], this.props)
-    const tracks = R.pathOr(null, ['playList', 'data', 'tracks'], this.props)
+    axios.defaults.headers.common['Access-Control-Allow-Origin'] = `Bearer ${token}`;
+    
+    const name = R.pathOr('', ['playListSpotify', 'data', 'name'], this.props)
+    const tracks = R.pathOr(null, ['playListSpotify', 'data', 'tracks'], this.props)
     
     return (
       <div>
         <h1>PlayList</h1>
+        <MusicList name={name} tracks={tracks} player="spotify"/>
         <MusicList name={name} tracks={tracks} player="spotify"/>
       </div>
     )
@@ -25,7 +27,25 @@ class PlayListInfo extends React.Component {
   
   componentDidMount() {
     const { dispatch } = this.props
-    dispatch(fetchData({url: `${playListInfo}${spotifyPlaylist}`, key: 'playList'}))
+
+    const dataDeezer = {"playlist_id":"6853097204","start":"0","nb":"500"}
+    var proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+    //     targetUrl = playListInfoDeezer
+    // fetch(proxyUrl + targetUrl, {method: 'POST', body: JSON.stringify(dataDeezer)})
+    //   .then(blob => blob.json())
+    //   .then(data => {
+    //     console.table(data);
+    //     return data;
+    //   })
+    //   .catch(e => {
+    //     console.log(e);
+    //     return e;
+    //   });
+
+
+    // fetch(playListInfoDeezer, {method: 'POST', body: JSON.stringify(dataDeezer),  mode: 'no-cors' }).then(r => console.log(r))
+    dispatch(fetchData({url: `${proxyUrl}${playListInfoDeezer}`, key: 'playListDeezer', data: dataDeezer, method:'POST'}))
+    // dispatch(fetchData({url: `${playListInfoSpotify}`, key: 'playListSpotify'}))
   }
 }
 
--- utils/routes.js
+++ utils/routes.js
@@ -1,4 +1,11 @@
 const baseURL = 'https://api.spotify.com/v1/'
+const spotifyPlaylist = '7mqbR7yHdisOwfKsaJtdLi'
 
 export const authEndpoint = `https://accounts.spotify.com/authorize`;
-export const playListInfo = `${baseURL}playlists/`
+export const playListInfoSpotify = `${baseURL}playlists/${spotifyPlaylist}`
+
+// Request Deezer
+const api_key = `ZAIVAHCEISOHWAICUQUEXAEPICENGUAFAEZAIPHAELEEVAHPHUCUFONGUAPASUAY`
+const version = `8.1.0`
+
+export const playListInfoDeezer = `https://api.deezer.com/1.0/gateway.php?version=${version}&api_key=${api_key}&output=3&buildId=ios11_universal&screenHeight=480&input=3&sid=frc43e3403521e32bc1325f9f0344c28da4d3de1&lang=br&network=d4a5fab109aa1b498dec92d4e3a85e69d36cc6e995be9e1c48523e9c98aa611e&mobile_tracking=eyJidW5kbGVfaWQiOiJjb20uZGVlemVyLkRlZXplciIsImlkZmEiOiJDNzVCMzkzMS0zM0I0LTQxMkUtQjUzMi1BODRDQjA2NjdEM0YiLCJhZGlkIjoiOWIzODBmM2NiMWJiZTZlODUwMjQxN2EyZjA0ZDM4MWMifQ==&screenWidth=320&method=playlist.getSongs`
